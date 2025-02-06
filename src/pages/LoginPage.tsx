33f851b98b8a0605288b33ae1ba0925e185cd82a
Yezen Armout <58712447+yarmout@users.noreply.github.com>
2025-02-05 22:13:51 -0500

    Add login form and authentication logic to LoginPage
    Introduced state management for name and email fields, a login API request using Axios, and navigation to the search page upon successful login. Updated the UI to include a form with inputs for user credentials and a login button. Added error handling for failed login attempts.

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,9 +1,41 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
 function LoginPage() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const navigate = useNavigate();
+
+    const handleLogin = async () => {
+        try {
+            await axios.post("https://frontend-take-home-service.fetch.com/auth/login",
+                {name, email },
+                { withCredentials: true }
+            );
+            navigate("/search");
+        } catch (error) {
+            console.log("Login failed", error);
+        }
+    };
 
     return (
-        <>
-            LoginPage
-        </>
+        <div>
+            <h1>Login</h1>
+            <input
+                type="text"
+                placeholder="Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                />
+            <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            />
+            <button onClick={handleLogin}>Login</button>
+        </div>
     );
 }
 
